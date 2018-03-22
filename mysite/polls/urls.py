1eac15ff38f4bbc7f3a26e89faef8f172d644f7d
Richie Wong <richiewong07@yahoo.com>
2018-03-21 19:50:05 -0500

    updated

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,18 @@ from . import views
 
 urlpatterns = [
 
-    # r^$ MEANS DON'T ADD ANYTHING TO OUR URL --> 127.0.0.1/polls/
+    # r^$ MEANS DON'T ADD ANYTHING TO OUR URL
     # views.index  IS WHAT YOU WANT TO DISPLAY
+    # 127.0.0.1/polls/
     url(r'^$', views.index, name="index"),
+
+    # SET QUESTION_ID TO A NUMBER
+    # 127.0.0.1/polls/1
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+
+    # 127.0.0.1/polls/1/results
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+
+    # 127.0.0.1/polls/1/votes
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 # HTTP RESPONSE --> ADDS BASIC TEXT TO YOUR WEBSITE
 def index(request):
-    return HttpResponse('Awesome Job guys! This is the index page, of our polls application')
+
+    # IMPORTED pub_date FROM Question Class FROM Models
+    # GET QUESTION OBJECTS AND ORDER THEM BY DATE
+    # JOIN THEM OUT OUTPUT AND RETURN IT IN HTTP REQUEST
+    lastest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in lastest_questions)
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question: %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
